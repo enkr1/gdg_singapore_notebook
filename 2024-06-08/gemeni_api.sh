ec976cf6e0d9c410ee42ea320d28073e6f7f185b
Jing Hui PANG (彭竞辉) <jinghuipang99@gmail.com>
2024-06-23 03:28:13 +0800

    Update gemeni_api.sh

--- 2024-06-08/gemeni_api.sh
+++ 2024-06-08/gemeni_api.sh
@@ -1,6 +1,7 @@
-
-
+# Put the key
+API_KEY=""
+# Curl it
 curl \
   -H 'Content-Type: application/json' \
-  -d '{"contents":[{"parts":[{"text":"Explain how AI works"}]}]}' \
+  -d '{"contents":[{"parts":[{"text":"Can you view images?"}]}]}' \
   -X POST "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${API_KEY}" | jq
